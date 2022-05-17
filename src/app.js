749f86c55a0c9362eeff02cddd059ad054afbe47
Shawn Faber <96348445+sfaber02@users.noreply.github.com>
2022-05-17 18:36:17 -0400

    finished basic html structure

--- src/app.js
+++ src/app.js
@@ -1,6 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './style.css';
 
-const App = () => <h1>HELLO</h1>
+const App = () => {
+    //State for storing responses from API
+    const [responses, setResponses] = useState(() => []);
+    
+
+    return (
+        <div id='mainContainer'>
+            <h1>Fun with AI</h1>
+            <form>
+                <label>Enter Prompt</label>
+                <input type='textarea' />
+                <input type='submit' />
+                <h1>Responses</h1>
+
+            </form>
+            
+        </div>
+    )
+}
 
 export { App };
