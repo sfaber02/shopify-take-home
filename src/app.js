2f3864511ab8cf438cbafbb7d7f2ea63efe0ab29
Shawn Faber <96348445+sfaber02@users.noreply.github.com>
2022-05-18 15:50:42 -0400

    added local storage feature to store past responses

--- src/app.js
+++ src/app.js
@@ -1,21 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-
+import { v4 as uuidv4 } from 'uuid';
 
 import './style.css';
 
+const LOCAL_STORAGE_KEY = 'hal.responses';
+let storedResponses = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+console.log(storedResponses);
+if (!storedResponses) storedResponses = [];
+
+
 const App = () => {
     //State for storing responses from API
-    const [responses, setResponses] = useState(() => []);
+    const [responses, setResponses] = useState(storedResponses);
 
     //click handler for submit button
     const handleSubmit = async e => {
         e.preventDefault();
         if (e.target.textInput.value) {
+            const prompt = e.target.textInput.value;
+            e.target.textInput.value = '';
 
             //body for API post request
             const data = JSON.stringify({
-                "prompt": e.target.textInput.value,
+                "prompt": prompt,
                 "max_tokens": 2000,
                 "temperature": 1,
                 "top_p": 1,
@@ -39,12 +47,11 @@ const App = () => {
                 setResponses(prev => 
                     [
                         {
-                            prompt: e.target.textInput.value, 
+                            prompt, 
                             response: response.data.choices[0].text
                         }, 
                         ...prev
-                    ])
-                    e.target.textInput.value = '';  
+                    ]) 
             })
             .catch(function (error) {
                 console.log(error);
@@ -54,25 +61,36 @@ const App = () => {
         }
     }
 
+    //store new responses in local storage
+    useEffect(() => {
+        console.log ('1');
+        localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(responses));
+    }, [responses]);
+    
+    //clear responses click handler
+    const clearResponses = () => setResponses([]);
+
+
     return (
         <div id='mainContainer'>
             <h1>HAL 9001</h1>
             <form onSubmit={handleSubmit} id="inputForm">
                 <label>Enter Prompt</label>
-                <input type='textarea' id='textInput' rows="10" cols="50" />
+                <input type='textarea' id='textInput' />
                 <input type='submit'  id='submitButton' />
             </form>
             {responses.length > 0 &&
                 <div id="responseContainer">
                     {responses.map(e => {
                         return (
-                            <div key={Date.now()} className="responses">
-                                <p>{e.prompt}</p>
+                            <div key={uuidv4()} className="responses">
+                                <p>Prompt: {e.prompt}</p>
                                 <br />
-                                <p>{e.response}</p>
+                                <p>Response: {e.response}</p>
                             </div>
                         )
-                    })}    
+                    })} 
+                    <button onClick={clearResponses}>Clear Responses</button>   
                 </div>
             }
         </div>
